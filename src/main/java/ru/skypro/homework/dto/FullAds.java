a21844367c33c26b149507f1d1ba6b3745733789
Sascvotch <sascvotch@yandex.ru>
2022-10-29 18:07:53 +0300

    Созданы классы и контроллер для объявлений

---
+++ src/main/java/ru/skypro/homework/dto/FullAds.java
@@ -0,0 +1,16 @@
+package ru.skypro.homework.dto;
+
+import lombok.Data;
+
+@Data
+public class FullAds {
+    private String authirFirstName;
+    private String autorLastName;
+    private String description;
+    private String email;
+    private String image;
+    private String phone;
+    private Integer pk;
+    private Integer price;
+    private String title;
+ }
