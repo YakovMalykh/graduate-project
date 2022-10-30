8b24561289a02ed54d9f05c451d80e0e44613612
Sascvotch <sascvotch@yandex.ru>
2022-10-30 20:34:13 +0300

    Добавлено подключениек ДБ

--- src/main/resources/liquibase/scripts/tables.sql
+++ src/main/resources/liquibase/scripts/tables.sql
@@ -4,28 +4,29 @@
 
 CREATE TABLE IF NOT EXISTS users
 (
-    user_id   BIGINT PRIMARY KEY,
+    user_id   SERIAL PRIMARY KEY,
     user_first_name VARCHAR (100) NOT NULL,
     user_last_name VARCHAR (100) NOT NULL,
     user_phone_number VARCHAR(20) NOT NULL,
     user_email VARCHAR (30) UNIQUE NOT NULL,
-    user_password VARCHAR (20) NOT NULL,
+    user_password VARCHAR (255) NOT NULL,
     user_role VARCHAR (10) NOT NULL
     );
 CREATE TABLE IF NOT EXISTS ads
 (
-    ads_id   BIGINT PRIMARY KEY,
+    ads_id  SERIAL PRIMARY KEY,
     author_id BIGINT REFERENCES users (user_id),
-    ads_image VARCHAR (100),
+    ads_image VARCHAR (250),
     ads_title VARCHAR (100) NOT NULL,
-    ads_description VARCHAR (250),
-    ads_price VARCHAR (30) NOT NULL
+    ads_description TEXT,
+    ads_price INTEGER NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS comment
 (
-    comment_id   BIGINT PRIMARY KEY,
+    comment_id   SERIAL PRIMARY KEY,
+    author_id BIGINT REFERENCES users (user_id),
     ads_id BIGINT REFERENCES ads (ads_id),
     comment_created TIMESTAMP NOT NULL,
-    comment_text VARCHAR (30) NOT NULL
+    comment_text TEXT NOT NULL
     );
