a91340f3be088ba1c4beac43cbbb04541e40878d
Sascvotch <sascvotch@yandex.ru>
2022-11-05 15:02:40 +0300

    ХЗ чего вобще происходит

--- src/main/java/ru/skypro/homework/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.skypro.homework.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.skypro.homework.models.User;
-
-public interface UserRepository extends JpaRepository <User, Long> {
-}
