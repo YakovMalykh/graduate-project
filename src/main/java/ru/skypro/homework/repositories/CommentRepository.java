39e65834470942ba9e1f511ca944b46bc60a239b
YakovMalykh <to-yakoff@mail.ru>
2022-11-16 19:48:46 +0300

    прописана авторизация, расставлены ограничения по по правам доступа к разным методам

--- src/main/java/ru/skypro/homework/repositories/CommentRepository.java
+++ src/main/java/ru/skypro/homework/repositories/CommentRepository.java
@@ -1,11 +1,12 @@
 package ru.skypro.homework.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.skypro.homework.models.Ads;
 import ru.skypro.homework.models.Comment;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 
-    List<Comment> findAllByAdsId(Long adsId);
+    List<Comment> findAllByAdsId(Ads adsId);
 }
