16fe64ff1d6eaff59045f1dd58e36d48f5cad8ea
Sascvotch <sascvotch@yandex.ru>
2022-12-02 21:26:30 +0300

    исправлена пара ошибок

--- src/main/java/ru/skypro/homework/dto/RegisterReqDto.java
+++ src/main/java/ru/skypro/homework/dto/RegisterReqDto.java
@@ -12,7 +12,8 @@ import javax.validation.constraints.Pattern;
 public class RegisterReqDto {
     @NotBlank
    private String firstName;
-    @NotBlank private String lastName;
+    @NotBlank
+    private String lastName;
     @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}" ,
             flags = Pattern.Flag. CASE_INSENSITIVE ,message = "Email должен быть корректным адресом электронной почты")
     private String username;// here email is used
